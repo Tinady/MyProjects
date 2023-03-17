d7942c054e2bc99df8368494f7415dba1ad79c26
Tinady <88823243+Tinady@users.noreply.github.com>
2023-03-17 21:48:43 +0300

    Add files via upload

---
+++ Quiz App/script.js
@@ -0,0 +1,26 @@
+const  quizData= [
+    {
+        question:"How old are u?",
+        a_1:19,
+        a_2:29,
+        a_3:39,
+        a_4:49,
+         },
+    
+         {
+            question:"What is your favourite coloe",
+            a_1:'green',
+            a_2:'blue',
+            a_3:'yellow',
+            a_4:'pink',
+             },
+
+       {
+                question:"what is my name",
+                 a_1:'green',
+                 a_2:'blue',
+                 a_3:'yellow',
+                 a_4:'pink',
+             },
+                
+]
